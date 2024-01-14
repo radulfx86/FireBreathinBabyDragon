e21659b83ed591a5af8ef6f6b4607db58df4fd27
Frank Ehmann <frank.ehmann@gmail.com>
2024-01-14 11:18:40 +0100

    more tile-stuff

--- src/tileDemo.cpp
+++ src/tileDemo.cpp
@@ -1,57 +1,117 @@
 #include "demos.h"
 #include <iostream>
+#include <vector>
 #include "raylib.h"
 
-void showTileDemo()
+typedef struct TileMap_t
 {
-    Texture tileMap = LoadTexture("images/tileMap.png");
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    const Vector2 tileSize = {16.0, 16.0};
-    const float scaleFactor = 4;
-    const int numScreenTilesX = screenWidth / (tileSize.x * scaleFactor);
-    const int numScreenTilesY = screenHeight / (tileSize.y * scaleFactor);
-    const int numMapTilesX = tileMap.width / tileSize.x;
-    const int numMapTilesY = tileMap.height / tileSize.y;
-    Rectangle tileSource[numScreenTilesX][numScreenTilesY];
+    Texture texture;
+    Vector2 tileSize;
+    Vector2 numTiles;
+    std::vector<Rectangle> sourceCoords;
+    std::vector<Rectangle> targetCoords;
+} TileMap_t;
+
+void drawTileMap(TileMap_t *tiles)
+{
+    /// dont draw if source and target sizes do not match
+    /// TODO find a better solution
+    if ( tiles->sourceCoords.size() != tiles->targetCoords.size() )
+    {
+        return;
+    }
+    for ( int i = 0; i < tiles->sourceCoords.size(); ++i )
+    {
+        DrawTexturePro(
+            tiles->texture, 
+            tiles->sourceCoords[i],
+            tiles->targetCoords[i],
+            {0.0,0.0},
+            0.0,
+            WHITE);
+    }
+}
+
+void initBackground(const int screenWidth, const int screenHeight, const float scaleFactor, TileMap_t *background)
+{
+    background->texture = LoadTexture("images/tileMap.png");
+    background->tileSize = {16.0, 16.0};
+    background->numTiles = {4, 4};
+
+    const int numScreenTilesX = screenWidth / (background->tileSize.x * scaleFactor);
+    const int numScreenTilesY = screenHeight / (background->tileSize.y * scaleFactor);
+
+    /// initialize a tile map for the screen
     for ( int y = 0; y < numScreenTilesY; ++y )
     {
         for ( int x = 0; x < numScreenTilesX; ++x )
         {
-            tileSource[x][y].width = tileSize.x;
-            tileSource[x][y].height = tileSize.y;
-            tileSource[x][y].x = tileSize.x * (x % numMapTilesX);
-            tileSource[x][y].y = tileSize.y * (y % numMapTilesY);
+            /// source rectangle in source-px-coordinates
+            background->sourceCoords.push_back({
+                3 * background->tileSize.x,
+                1 * background->tileSize.y,
+                background->tileSize.x,
+                background->tileSize.y});
+            /// target rectangle in target-px-coordinates (scaled)
+            background->targetCoords.push_back({
+                x * background->tileSize.x * scaleFactor,
+                y * background->tileSize.y * scaleFactor,
+                background->tileSize.x * scaleFactor,
+                background->tileSize.y * scaleFactor});
         }
     }
+}
+
+void initForeground(const int screenWidth, const int screenHeight, const float scaleFactor, TileMap_t *foreground)
+{
+    foreground->texture = LoadTexture("images/assets.png");
+    foreground->tileSize = {16.0, 32.0};
+    foreground->numTiles = {10, 5};
+    static const int MAX_ELEMENTS = 42;
+    for ( int i = 0; i < MAX_ELEMENTS; ++i )
+    {
+        float x = GetRandomValue(0, screenWidth - foreground->tileSize.x * scaleFactor);
+        float y = GetRandomValue(0, screenHeight - foreground->tileSize.y * scaleFactor);
+        foreground->sourceCoords.push_back({0,(i%(int)foreground->numTiles.y) * foreground->tileSize.y,foreground->tileSize.x, foreground->tileSize.y});
+        foreground->targetCoords.push_back({x,y,foreground->tileSize.x * scaleFactor, foreground->tileSize.y * scaleFactor});
+    }
+    // TODO: sort by y-value
+    // NOTE: nah, the type with split source/target-coords doesnt sort too well ...
+}
+
+void showTileDemo()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    const float scaleFactor = 4;
+    TileMap_t background;
+    initBackground(screenWidth, screenHeight, scaleFactor, &background);
+
+    TileMap_t foreground;
+    initForeground(screenWidth, screenHeight, scaleFactor, &foreground);
+
     SetWindowSize(screenWidth, screenHeight);
     SetWindowTitle("Tilemap Demo");
     //SetTargetFPS(10);
+    float sumDelta = 0.0;
+    int frameCnt = 0;
     while (!WindowShouldClose())
     {
-        BeginDrawing();
             float delta = GetFrameTime();
-            std::cout << "delta: " << delta << " s\n";
-            std::cout << "FPS: " << 1.0 / delta << "\n";
-            ClearBackground(BLACK);
-            Rectangle tileDest = {0.0, 0.0, tileSize.x * scaleFactor, tileSize.y * scaleFactor};
-            Vector2 origin = {0.0,0.0};
-            for ( int y = 0; y < numScreenTilesY; ++y )
+            sumDelta += delta;
+            ++frameCnt;
+            if ( sumDelta > 1.0 )
             {
-                for ( int x = 0; x < numScreenTilesX; ++x )
-                {
-//                    DrawTextureRec(tileMap, tileSource[x][y], Vector2({(float)(x * tileSize.x * scaleFactor), (float)(y * tileSize.y * scaleFactor)}), WHITE);
-                    tileDest.x = x * tileSize.x * scaleFactor;
-                    tileDest.y = y * tileSize.y * scaleFactor;
-                    DrawTexturePro(
-                        tileMap, 
-                        tileSource[x][y],
-                        tileDest,
-                        origin,
-                        0.0,
-                        WHITE);
-                }
+                std::cout << "FPS: " << frameCnt << "\n";
+                sumDelta -= 1.0;
+                frameCnt = 0;
             }
+        BeginDrawing();
+            ClearBackground(BLACK);
+            drawTileMap(&background);
+            drawTileMap(&foreground);
         EndDrawing();
     }
+    UnloadTexture(background.texture);
+    UnloadTexture(foreground.texture);
 }
